0408cd1e4bfbdb71b0c2412cacce225ecce27956
Vipul Kumar Singh <vipul24.9669@gmail.com>
2023-05-21 20:32:02 +0530

    Added cache annotations

--- src/main/java/com/vkstech/SpringDataRedis/service/impl/UserServiceImpl.java
+++ src/main/java/com/vkstech/SpringDataRedis/service/impl/UserServiceImpl.java
@@ -6,6 +6,9 @@ import com.vkstech.SpringDataRedis.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 
@@ -31,12 +34,14 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Cacheable(value = "users", key = "#userId", unless = "#result.followers < 12000")
     public User getUserById(String userId) {
         LOG.info("UserService::getUserById, userId = {}", userId);
         return userRepository.findById(Long.valueOf(userId)).get();
     }
 
     @Override
+    @CachePut(value = "users", key = "#user.id")
     public User updateUser(String userId, String name, long followers) {
         LOG.info("UserService::updateUser, userId = {}", userId);
         User user = getUserById(userId);
@@ -48,6 +53,7 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     @Modifying
+    @CacheEvict(value = "users", allEntries=true)
     public void deleteUserById(String userId) {
         LOG.info("UserService::deleteUserById, userId = {}", userId);
         userRepository.deleteById(Long.valueOf(userId));
